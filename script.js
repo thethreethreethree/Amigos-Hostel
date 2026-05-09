285ff430d6c6d04dc498925c83363bdd4ff965ca
WDT <johnsyramos@gmail.com>
2026-05-09 13:31:12 +0800

    Add scroll event listener to change navbar background

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// script.js
 
+window.addEventListener("scroll", () => {
+
+  const navbar = document.querySelector(".navbar");
+
+  if(window.scrollY > 50){
+
+    navbar.style.background = "rgba(255,255,255,0.95)";
+
+  } else {
+
+    navbar.style.background = "rgba(255,255,255,0.75)";
+
+  }
+
+});
